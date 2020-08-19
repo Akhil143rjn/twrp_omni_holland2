472b57e984ebd7076be90524a28fb120136c5910
akhil143rjn <akhil143rjn@gmail.com>
2020-08-19 17:27:00 +0000

    making tree more bugged

--- omni_holland2.mk
+++ omni_holland2.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8953
+
 # Inherit Telephony packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
